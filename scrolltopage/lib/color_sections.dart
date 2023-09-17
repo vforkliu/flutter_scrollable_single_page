40b0485d8599e6507648ee9206a5f160312d6f5f
liujun <vforkliu@gmail.com>
2023-09-17 12:22:26 +0800

    scroll to page finish

---
+++ scrolltopage/lib/color_sections.dart
@@ -0,0 +1,115 @@
+import 'package:common/extensions/color_extensions.dart';
+import 'package:common/model/shape_border_type.dart';
+import 'package:flutter/material.dart';
+import 'shape_border_listview.dart';
+import 'package:common/entity/color_code.dart';
+
+class ColorSections extends StatefulWidget {
+  final List<MaterialColor> colors;
+  final ValueNotifier<ShapeBorderType?> shapeBorderTypeNotifier;
+  final ValueNotifier<ColorCode?> colorCodeNotifier;
+
+  const ColorSections({
+    Key? key,
+    required this.colors,
+    required this.shapeBorderTypeNotifier,
+    required this.colorCodeNotifier,
+  }) : super(key: key);
+
+  @override
+  State<ColorSections> createState() => _ColorSectionsState();
+}
+
+class _ColorSectionsState extends State<ColorSections> {
+  final double _minPageHeight = 600;
+
+  PageController _pageController = PageController();
+
+  // Find the index of the color code from the colors list
+  int get _colorCodeIndex {
+    final hexColorCode = widget.colorCodeNotifier.value?.hexColorCode;
+    int index = widget.colors.indexWhere((element) {
+      return element.toHex() == hexColorCode;
+    });
+    return index > -1 ? index : 0;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.colorCodeNotifier.addListener(() {
+      final fromScroll =
+          widget.colorCodeNotifier.value?.source == ColorCodeSelectionSource.fromScroll;
+      if (_pageController.hasClients && !fromScroll) {
+        _pageController.animateToPage(
+          _colorCodeIndex,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final availableHeight = constraints.maxHeight;
+        _updatePageController(availableHeight);
+        return NotificationListener<ScrollNotification>(
+          onNotification: (notification) {
+            if (notification is UserScrollNotification) {
+              _onUserScroll();
+            }
+            return true;
+          },
+          child: _pageView(),
+        );
+      },
+    );
+  }
+
+  PageView _pageView() {
+    return PageView.builder(
+      pageSnapping: false, // snapping off
+      scrollDirection: Axis.vertical,
+      controller: _pageController,
+      itemCount: widget.colors.length,
+      physics: const AlwaysScrollableScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        final color = widget.colors[index];
+        return Container(
+          color: color.shade100,
+          child: ShapeBorderListView(
+            sectionColor: color,
+            shapeBorderTypeNotifier: widget.shapeBorderTypeNotifier,
+            colorCodeNotifier: widget.colorCodeNotifier,
+          ),
+        );
+      },
+    );
+  }
+
+  void _updatePageController(double availableHeight) {
+    if (availableHeight < _minPageHeight) {
+      _pageController = PageController(
+        viewportFraction: _minPageHeight / availableHeight,
+        initialPage: _colorCodeIndex,
+      );
+    } else {
+      _pageController = PageController(
+        viewportFraction: 1,
+        initialPage: _colorCodeIndex,
+      );
+    }
+  }
+
+  void _onUserScroll() {
+    final pageIndex = _pageController.page?.floor() ?? 0;
+    final hexColorCode = widget.colors[pageIndex].toHex();
+    widget.colorCodeNotifier.value = ColorCode(
+      hexColorCode: hexColorCode,
+      source: ColorCodeSelectionSource.fromScroll,
+    );
+  }
+}
